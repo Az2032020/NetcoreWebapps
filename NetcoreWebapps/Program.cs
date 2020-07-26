c923998f8529074e698094970ce755e454e7da07
Az203 <viswanathan0386@gmail.com>
2020-07-26 14:43:46 +0530

    commit 1

--- NetcoreWebapps/Program.cs
+++ NetcoreWebapps/Program.cs
@@ -13,7 +13,7 @@ namespace NetcoreWebapps
     {
         public static void Main(string[] args) 
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();   
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
